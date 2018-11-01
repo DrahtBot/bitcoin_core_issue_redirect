2badcb5ad914dbd9a9e48c89e1984c251e5c5737
MarcoFalke <falke.marco@gmail.com>
2018-11-01 14:05:05 -0400

    Add html redirect generator

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::io::Write;
+
+
+fn main() {
+    for i in 0..100 {
+        let file = std::fs::File::create(format!("r/{}.html", i)).unwrap();
+
+        let content = format!(
+            "<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv=refresh content=\"0; url=https://github.com/bitcoin/bitcoin/issues/{}\">
+</head>
+<body>
+</body>
+</html>",
+            i
+        );
+        std::io::BufWriter::new(&file).write(&content.as_bytes()).unwrap();
+    }
+}
